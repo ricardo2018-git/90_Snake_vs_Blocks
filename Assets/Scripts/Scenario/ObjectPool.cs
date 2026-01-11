050baaacc9bfdd11c3fdd6a80102f82075c797ab
Ricardo <rp_mira@hotmail.com>
2026-01-11 07:40:11 -0300

    Spawn de alguns objetos e deixa dentro de um vetor desativado

---
+++ Assets/Scripts/Scenario/ObjectPool.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    // Variaveis de Scripts
+    // ------------------------
+
+    // Variaveis de Componentes
+    // ------------------------
+
+    // Variaveis de Controle
+    public int amount;              // Qts de objs que vai ser instanciado do prefab
+    private int index;              // Index para controlar qual posição do vetor estamos
+    // ------------------------
+
+    // Variaveis de Armas, Ataque, Prefab, Game Object e Audio
+    public GameObject prefab;       // Referencia ao obj que vai ser instanciado
+    public GameObject[] prefabs;    // Vai guardar todos o prefab que forma instanciados
+    // ------------------------
+
+    // Variaveis de UI
+    // ------------------------
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        // Associa Scripts
+
+        // Associa Componentes
+
+        // Variaveis de Controle, metodos e funções
+        prefabs = new GameObject[amount];               // Inicializa o vetor com tamanho de amount
+        for(int i = 0; i < amount; i++)                 // Loop amount vezes
+        {
+            prefabs[i] = Instantiate(prefab, new Vector2(0, 15), Quaternion.identity);  // Instancia prefab dentro do vetor na posição i desse momento
+            prefabs[i].SetActive(false);    // Desativa prefab da cena
+        }
+
+        // Associa Armas, Ataque, Prefab e Game Object
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetObject()   // Acessa um prefab do vetor e retorna ele
+    {
+        index++;            // Soma Mais um ao index
+        if(index >= amount) // Verifica se index é maior ou igual ao amount. Ou seja verifica se esta na ultima posição
+        {
+            index = 0;      // Reseta index para primeira posição
+        }
+        prefabs[index].SetActive(true); // Ativa prefab na cena
+        return prefabs[index];          // Retorna prefab ativo
+    }
+}
